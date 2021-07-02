be09128690ed8e83c2081560a5709a0770c00e7f
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2021-07-02 17:02:16 +0530

    Update Find_Numbers_with_Even_Number_of_Digits.py

--- Find_Numbers_with_Even_Number_of_Digits.py
+++ Find_Numbers_with_Even_Number_of_Digits.py
@@ -1,5 +1,5 @@
 #Find Numbers with Even Number of Digits
-#solution for leetcode
+#"SOLUTION FOR LEETCODE"
 class Solution:
   def findNumbers(self, nums: List[int]) -> int:
     counter = 0
@@ -7,3 +7,27 @@ class Solution:
       if (len(str(num)) % 2 == 0):
         counter += 1
     return counter
+  
+#"WHEN YOU WOULD LIKE TO TAKE INPUT FROM USER"
+def findNumbers(nums):
+        counter = 0
+        for num in nums:
+            if (len(str(num)) %2 == 0):
+                counter += 1
+        return counter
+        
+#nums = [5555,9001,4822,1771]
+nums = [int(num) for num in input("").split()]
+print(findNumbers(nums))
+
+#"WHEN WE ARE TAKING DEFAULT INPUT"
+def findNumbers(nums):
+        counter = 0
+        for num in nums:
+            if (len(str(num)) %2 == 0):
+                counter += 1
+        return counter
+        
+nums = [5555,9001,4822,1771]
+#nums = [int(num) for num in input("").split()]
+print(findNumbers(nums))
