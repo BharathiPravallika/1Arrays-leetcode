38fa64756d25611fe04b89428e4190b8c62cfd1f
BharathiPravallika <52744796+BharathiPravallika@users.noreply.github.com>
2021-07-02 16:46:31 +0530

    Create Find_Numbers_with_Even_Number_of_Digits.py

---
+++ Find_Numbers_with_Even_Number_of_Digits.py
@@ -0,0 +1,9 @@
+#Find Numbers with Even Number of Digits
+#solution for leetcode
+class Solution:
+  def findNumbers(self, nums: List[int]) -> int:
+    counter = 0
+    for num in nums:
+      if (len(str(num)) % 2 == 0):
+        counter += 1
+    return counter
